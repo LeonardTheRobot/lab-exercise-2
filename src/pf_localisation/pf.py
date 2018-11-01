0c7a09a743fec615140e37f6db0ec214f048f96e
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2018-11-01 15:07:53 +0000

    Started implemening initial particle cloud generator

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -22,13 +22,36 @@ class PFLocaliser(PFLocaliserBase):
         
        
     def initialise_particle_cloud(self, initialpose):
-        # Set particle cloud to initialpose plus noise
+        # Distribute particles randomly across the map
+        number_of_particles = 300
+	    map_width = self.occupancy_map.info.width
+	    map_height = self.occupancy_map.info.height
+	
 
- 
-    
+        for i in range(number_of_particles):
+            pose = Pose()
+            x = random.randint(0, 20 * map_width) / 20.0
+            y = random.randint(0, 20 * map_height) / 20.0
+            z = random.uniform(0, 6.283)
+            pose.position.x = x
+            pose.position.y = y
+            pose.orientation = rotateQuaternion(Quaternion(w=1.0),z)
+
+            # Remove particles that are outside map boundaries
+            x_index = x / 0.05
+            y_index = y / 0.05
+
+            major_index = x_index + (y_index * map.width)
+            valid = self.occupancy_map.data[major_index]
+
+            if valid == 0 or valid == -1:
+                continue
+            else:
+                self.particlecloud.poses.add(pose)
+
+
     def update_particle_cloud(self, scan):
         # Update particlecloud, given map and laser scan
-
 
     def estimate_pose(self):
         # Create new estimated pose, given particle cloud
